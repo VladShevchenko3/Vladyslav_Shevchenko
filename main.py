de1e34a37f361012eedaadcff2770727ae1ffa10
vlad3a9c <79662400+vlad3a9c@users.noreply.github.com>
2021-10-24 22:23:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def convert(i):
      st=str(bin(i+2**32)[-32:])
      st1 = st[:8]
-     st2 = st[8:16]
+     st2 == st[8:16]
      st3 = st[16:24]
      st4 = st[24:]
      s = str(int(st1, 2)) + "." + str(int(st2, 2)) + "." + str(int(st3, 2)) + "." + str(int(st4, 2))
